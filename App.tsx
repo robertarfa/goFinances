97b827a74392a443db9392555f7d959f5d567e36
Roberta Aguilar <roberta.aguilar@kobel.com.br>
2021-06-30 18:35:09 -0300

    feat: new app goFinance

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Dashboard } from './src/screens/Dashboard';
+
+export default function App() {
+  return <Dashboard/>
+}
+
