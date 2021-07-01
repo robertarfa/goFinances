1c6cce2dc2e7effb4310299ef28d7a61f23bdca8
Roberta Aguilar <roberta.aguilar@kobel.com.br>
2021-07-01 19:39:48 -0300

    feat: iniciando o Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,42 @@
 import React from 'react'
+import { HighlightCard } from '../../components/HighlightCard'
 
-import { Container, Title } from './styles'
+
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon
+} from './styles'
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>
-                Dashboard
-            </Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/22533625?v=4' }}
+                        />
+
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Roberta</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon
+                        name='power'
+                    />
+                </UserWrapper>
+            </Header >
+
+            <HighlightCard />
         </Container>
     )
 }
