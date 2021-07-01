56b10caa173201fc3f09946a3800e451d18da999
Roberta Aguilar <roberta.aguilar@kobel.com.br>
2021-07-01 08:49:28 -0300

    feat: configurando theme

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,10 +4,13 @@ export const Container = styled.View`
 flex: 1;
 justify-content: center;
 align-items: center;
+
+background-color: ${({ theme }) => theme.colors.background}
 `
 
 export const Title = styled.Text`
+font-family: ${({ theme }) => theme.fonts.bold};
 font-size: 24px;
 font-weight:bold;
-color:purple;
+color:${({ theme }) => theme.colors.title};
 `
