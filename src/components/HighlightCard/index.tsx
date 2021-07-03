af82cc789d3499c5685bbdb5c4774c121d12e636
Roberta Aguilar <roberta.aguilar@kobel.com.br>
2021-07-03 09:03:36 -0300

    feat: adicionando props nos components

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -10,25 +10,45 @@ import {
     LastTransaction,
 } from './styles'
 
+interface Props {
+    type: 'up' | 'down' | 'total';
+    title: string;
+    amount: string;
+    lastTransaction: string;
+}
 
-export function HighlightCard() {
+const icon = {
+    up: 'arrow-up-circle',
+    down: 'arrow-down-circle',
+    total: 'dollar-sign',
+}
+
+export function HighlightCard({
+    type,
+    title,
+    amount,
+    lastTransaction,
+}: Props) {
     return (
-        <Container>
+        <Container type={type}>
 
             <Header>
-                <Title>
-                    Entrada
+                <Title type={type}>
+                    {title}
                 </Title>
-                <Icon name="arrow-up-circle" />
+                <Icon
+                    name={icon[type]}
+                    type={type}
+                />
             </Header>
 
             <Footer>
-                <Amount>
-                    R$ 17.400,00
+                <Amount type={type}>
+                    {amount}
                 </Amount>
 
-                <LastTransaction>
-                    Última entrada dia 13 de abril
+                <LastTransaction type={type}>
+                    {lastTransaction}
                 </LastTransaction>
             </Footer>
 
