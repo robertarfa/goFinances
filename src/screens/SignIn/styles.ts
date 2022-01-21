9ab010787d6ad857a82e22ee4d159b946c8fd968
roberta_rfa <roberta_rfa@hotmail.com>
2022-01-20 22:25:18 -0300

    feat: signIn button

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,4 +1,4 @@
-import { RFValue } from 'react-native-responsive-fontsize'
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
@@ -42,4 +42,10 @@ width:100%;
 height:30%;
 
 background-color: ${({ theme }) => theme.colors.secondary}
+`
+export const FooterWrapper = styled.View`
+margin-top:${RFPercentage(-4)}px;
+
+padding: 0 32px;
+justify-content:space-between;
 `
